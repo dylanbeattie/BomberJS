b90208847aa5d2c013f4885bc2d5f7dca2053ea6
Dylan Beattie <dylan@dylanbeattie.net>
2011-03-24 22:42:07 +0000

    Initial commit - got log & chat code working; about to refactor client code into MVC-style architecture

---
+++ bomber-server.js
@@ -0,0 +1,89 @@
+var sys = require("sys");
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+var ws = require('./lib/ws/server');
+
+/* Create a static file server */
+var server = http.createServer(function (request, response) {
+	var uri = url.parse(request.url).pathname;
+	var filename = path.join(process.cwd(), uri);
+	if (/\/$/.test(filename)) filename += 'index.html';
+
+	path.exists(filename, function (exists) {
+
+		sys.puts(filename);
+		if (!exists) {
+			response.statusCode = 404;
+			response.setHeader("Content-Type", "text/html");
+			filename = path.join(process.cwd(), "/errors/404.html");
+			fs.readFile(filename, "binary", function (err, file) {
+				if (err) {
+					response.write('<h1>404 File Not Found</h1>');
+					response.write('<p>To replace this error, create /errors/404.html in your nodeJS project</p>');
+					response.write('<hr />');
+					response.write('<p>This is a deliberately verbose error message because if it is below a certain threshold,');
+					response.write('your browser will ignore it and make it look like the interwebs are broken instead.</p>');
+					response.write('<p>This can be confusing when dealing with an experimental web server written in JavaScript.</p>');
+					response.write('<hr />');
+					response.write('<p>This is a deliberately verbose error message because if it is below a certain threshold,');
+					response.write('your browser will ignore it and make it look like the interwebs are broken instead.</p>');
+					response.write('<p>This can be confusing when dealing with an experimental web server written in JavaScript.</p>');
+					response.write('<hr />');
+					response.write('<p>This is a deliberately verbose error message because if it is below a certain threshold,');
+					response.write('your browser will ignore it and make it look like the interwebs are broken instead.</p>');
+					response.write('<p>This can be confusing when dealing with an experimental web server written in JavaScript.</p>');
+					response.end();
+					return;
+				}
+				response.write(file, "binary");
+				response.end();
+			});
+			return;
+		}
+
+		fs.readFile(filename, "binary", function (err, file) {
+			if (err) {
+				response.statusCode = 500;
+				response.setHeader("Content-Type", "text/plain");
+				response.write(err + "\n");
+				response.end();
+				return;
+			}
+			response.statusCode = 200;
+			response.write(file, "binary");
+			response.end();
+		});
+	});
+})
+
+
+var socketServer = ws.createServer({ server: server, debug: true });
+// Handle WebSocket Requests
+socketServer.addListener("connection", function (conn) {
+	conn.send("Connection: " + conn.id);
+
+	conn.addListener("message", function (message) {
+		conn.broadcast("<" + conn.id + "> " + message);
+
+		if (message == "error") {
+			conn.emit("error", "test");
+		}
+	});
+});
+
+socketServer.addListener("error", function () {
+	console.log(Array.prototype.join.call(arguments, ", "));
+});
+
+socketServer.addListener("disconnected", function (conn) {
+	server.broadcast("<" + conn.id + "> disconnected");
+});
+
+// server.listen(8081);
+
+// sys.puts('Started BomberJS socket server on port 8081');
+var port = 8000;
+server.listen(port);
+sys.puts("Bomber server running at http://192.168.1.8:" + port + "/");
---
+++ client/script/bomber-client.js
@@ -0,0 +1,60 @@
+﻿function pad(n) {
+	return n < 10 ? '0' + n.toString(10) : n.toString(10);
+}
+
+function timestamp() {
+	var d = new Date();
+	return [pad(d.getHours()), pad(d.getMinutes()), pad(d.getSeconds()), (d.getTime() + "").substr(-4, 4)].join(':');
+};
+
+function log(data) {
+	output_log.innerHTML += timestamp() + ": " + data + "<br />";
+}
+
+var conn, recvd, connections = 0;
+var output_log = document.getElementById("log");
+var connect = function () {
+	if (window["WebSocket"]) {
+		recvd = 0;
+		host = (document.location.host != "" ? document.location.host : "localhost:8000");
+		conn = new WebSocket("ws://" + host + "/test");
+		conn.onmessage = function (evt) { log(evt.data); };
+		conn.onerror = function () { log("error", arguments); };
+		conn.onclose = function () { log("closed"); };
+		conn.onopen = function () { log("opened"); };
+	}
+};
+
+$(function () {
+	$("#send").click(function () {
+		if (conn) {
+			setTimeout(function () {
+				conn.send("test message");
+				log("<" + conn.id + "> " + "test message");
+			}, 0);
+		}
+		return false;
+	});
+
+	$("#close").click(function () {
+		if (conn) conn.close();
+		return (conn = false);
+	});
+
+	$("#open").click(function () {
+		if (!conn) connect();
+		return false;
+	});
+	$("#chat-form").submit(function () {
+		var message = $("#chat-line").val();
+		if (message) {
+			setTimeout(function () {
+				conn.send(message);
+			}, 0);
+			$("#chat-line").val('');
+		}
+		return (false);
+	});
+
+	connect();
+});
